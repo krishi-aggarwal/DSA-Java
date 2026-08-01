38586c8ab08c25fdab1c7e71eff0d96f2a510f2b
krishi-aggarwal <krishi.2004.aggarwal@gmail.com>
2026-08-01 19:13:42 +0530

    Leetcode 1920 : Build Array from Permutation

---
+++ Arrays/BuildArray.java
@@ -0,0 +1,22 @@
+// Leetcode : 1920 Build Array from Permutation
+
+// Time : O(n)
+
+// Space : O(n)
+
+package Arrays;
+
+class BuildArray {
+    public static void main(String[] args){
+        int[] nums = {0,2,1,5,3,4};
+        int[] ans = new int[nums.length];
+
+        for(int i=0;i<nums.length;i++){
+            ans[i] = nums[nums[i]];
+        }
+        for(int i : ans){
+            System.out.print(i);
+        }
+        
+    }
+}
