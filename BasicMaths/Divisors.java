69e97bbcef0a6d7641222b94e0c66ab0adc27fb8
krishi-aggarwal <krishi.2004.aggarwal@gmail.com>
2026-08-04 21:31:13 +0530

    Feat - Solve Print all Divisors

--- BasicMaths/Divisors.java
+++ BasicMaths/Divisors.java
@@ -4,9 +4,12 @@ public class Divisors {
     public static void main(String[] args) {
         int n = 36;
 
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<=Math.sqrt(n);i++){
             if(n%i == 0){
-                System.out.print(i+" ");
+                System.out.println(i);
+                if((n/i)!=i){
+                    System.out.println(n/i);
+                }
             }
         }
     }
