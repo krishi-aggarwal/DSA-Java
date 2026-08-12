6c03c2f57a4f46e1a59222e8a059720ea7f5bbf6
krishi-aggarwal <krishi.2004.aggarwal@gmail.com>
2026-08-12 12:05:44 +0530

    update: Print all Divisors (Ordered)

--- BasicMaths/Divisors.java
+++ BasicMaths/Divisors.java
@@ -1,16 +1,23 @@
 // TC : O(N)
-
+import java.util.*;
 public class Divisors {
     public static void main(String[] args) {
         int n = 36;
+        List<Integer> l = new ArrayList<Integer>();
 
         for(int i=1;i<=Math.sqrt(n);i++){
             if(n%i == 0){
-                System.out.println(i);
+                l.add(i);
+                //System.out.println(i);
+
                 if((n/i)!=i){
-                    System.out.println(n/i);
+                    l.add(n/i);
+                    //System.out.println(n/i);
                 }
+
             }
         }
+        l.sort(null);
+        System.out.println(l);
     }
 }
