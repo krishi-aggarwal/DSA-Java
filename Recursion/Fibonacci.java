b1bd30459eeddff59da5caea3fe344003c786919
krishi-aggarwal <krishi.2004.aggarwal@gmail.com>
2026-08-14 15:32:33 +0530

    Feat: Leetcode 509 : Fibonacci Number

---
+++ Recursion/Fibonacci.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class Fibonacci {
+    
+    public static int fibo(int n){
+        if(n<=1){
+            return n;
+        }
+        int last = fibo(n-1);
+        int slast = fibo(n-2);
+
+        return last+slast;  
+    }
+    public static void main(String[] args) {
+        int n = 3 ;
+        System.out.println(fibo(n));
+    }
+}
